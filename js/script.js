677fcfeae993988572d7f60182e409e683bf6700
Carlos Caf <122706392+carloscaf72@users.noreply.github.com>
2023-01-16 23:09:50 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let x = document.querySelector('h2');
+console.log(x);
+console.log(typeof x);
+console.log(x.innerHTML);
+
+let h2 = document.getElementsByClassName('qualquer');
+console.log(typeof h2);
+console.log(h2); // retorna um HTMLCollection
+console.log(h2.innerHTML); // indefined
